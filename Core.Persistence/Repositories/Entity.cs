c267fc96d9cc1bb7accd5986942d6c7cdb5a1c6a
Murat Ilgun <muratilgun34@gmail.com>
2024-02-27 00:46:43 +0300

    Add project files.

---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    public Entity()
+    {
+        Id = default!;
+    }
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
