212b2df5dc5da14f382b172194de39adcbd2cec1
Murat Ilgun <muratilgun34@gmail.com>
2024-02-28 23:58:11 +0300

    Added IEntityTimestamps

---
+++ Core.Persistence/Repositories/IEntityTimestamps.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories;
+public interface IEntityTimestamps
+{
+    DateTime CreatedAt { get; set; }
+    DateTime? UpdatedAt { get; set; }
+    DateTime? DeletedAt { get; set; }
+}
