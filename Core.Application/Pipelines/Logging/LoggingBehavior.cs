5f2b38f5e8429194450d1cf82cde14b9b01360aa
Murat Ilgun <muratilgun34@gmail.com>
2024-03-10 20:00:46 +0300

    Logging Behavior

--- Core.Application/Pipelines/Logging/LoggingBehavior.cs
+++ Core.Application/Pipelines/Logging/LoggingBehavior.cs
@@ -6,36 +6,36 @@ using System.Text.Json;
 
 namespace Core.Application.Pipelines.Logging;
 public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : IRequest, ILoggableRequest
+    where TRequest : IRequest<TResponse>, ILoggableRequest
 {
+
     private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly LoggerServiceBase _loggerServiceBase;
 
-    public LoggingBehavior(LoggerServiceBase loggerServiceBase, IHttpContextAccessor httpContextAccessor)
+    public LoggingBehavior(IHttpContextAccessor httpContextAccessor, LoggerServiceBase loggerServiceBase)
     {
-        _loggerServiceBase = loggerServiceBase;
         _httpContextAccessor = httpContextAccessor;
+        _loggerServiceBase = loggerServiceBase;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
-        List<LogParameter> logParameters = new()
-        {
-            new LogParameter{
-                Type= request.GetType().Name,
-                Value = request
-            }
-        };
-        LogDetail logDetail = new()
-        {
+        List<LogParameter> logParameters =
+            new()
+            {
+                new LogParameter{Type= request.GetType().Name, Value= request },
+            };
 
-            MethodName = request.GetType().Name,
-            Parameters = logParameters,
-            FullName = request.GetType().FullName ?? string.Empty,
-            User = _httpContextAccessor.HttpContext?.User.Identity?.Name?? "Anonymous"
-        };
+        LogDetail logDetail
+            = new()
+            {
+                MethodName = next.Method.Name,
+                Parameters = logParameters,
+                User = _httpContextAccessor.HttpContext.User.Identity?.Name ?? "Anonymous",
+                FullName = next.Method.DeclaringType?.FullName ?? string.Empty,
+            };
 
         _loggerServiceBase.Info(JsonSerializer.Serialize(logDetail));
-        return await  next();
+        return await next();
     }
 }
