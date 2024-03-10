d7b90baafbbc2d16249d8a77aa7572f746e95f5f
Murat Ilgun <muratilgun34@gmail.com>
2024-03-11 02:19:45 +0300

    Added EmailAuthenticator

---
+++ Core.Security/Entities/EmailAuthenticator.cs
@@ -0,0 +1,28 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Core.Security.Entities;
+public class EmailAuthenticator : Entity<Guid>
+{
+    public Guid UserId { get; set; }
+    public string? ActivationKey{ get; set; } 
+    public bool IsVerified { get; set; }
+    public virtual User User { get; set; } = null!;
+
+    public EmailAuthenticator()
+    {
+        
+    }
+
+    public EmailAuthenticator (Guid userId, bool isVerified)
+    {
+        UserId = userId;
+        IsVerified = isVerified;
+    }
+
+    public EmailAuthenticator(Guid id, Guid userId, bool isVerified) : base(id)
+    {
+        UserId = userId;
+        IsVerified = isVerified;
+    }
+
+}
