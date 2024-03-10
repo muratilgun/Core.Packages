4ae45b185b7330665457d2ff01ebfe519d7995f8
Murat Ilgun <muratilgun34@gmail.com>
2024-03-10 15:41:08 +0300

    Added LogDetail

---
+++ Core.CrossCuttingConcerns/Logging/LogParameter.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.CrossCuttingConcerns.Logging;
+public class LogParameter
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+    public string Type { get; set; }
+
+    public LogParameter()
+    {
+        Name = string.Empty;
+        Value = string.Empty;
+        Type = string.Empty;
+    }
+
+    public LogParameter(string name, string value, string type)
+    {
+        Name = name;
+        Value = value;
+        Type = type;
+    }
+
+}
