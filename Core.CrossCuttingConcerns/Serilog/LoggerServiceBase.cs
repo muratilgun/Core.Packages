ece8575acd679638a564967ca3742eccb3951dcd
Murat Ilgun <muratilgun34@gmail.com>
2024-03-10 16:00:32 +0300

    Aded Serilog file logger

---
+++ Core.CrossCuttingConcerns/Serilog/LoggerServiceBase.cs
@@ -0,0 +1,18 @@
+﻿
+using Serilog;
+
+namespace Core.CrossCuttingConcerns.Serilog;
+public abstract class LoggerServiceBase
+{
+    protected ILogger Logger { get; set; }
+    protected LoggerServiceBase() => Logger = null;
+    protected LoggerServiceBase(ILogger logger) => Logger = logger;
+
+    protected void Verbose(string message) => Logger.Verbose(message);
+    protected void Fatal(string message) => Logger.Fatal(message);
+    protected void Info(string message) => Logger.Information(message);
+    protected void Warn(string message) => Logger.Warning(message);
+    protected void Debug(string message) => Logger.Debug(message);
+    protected void Error(string message) => Logger.Error(message);
+
+}
