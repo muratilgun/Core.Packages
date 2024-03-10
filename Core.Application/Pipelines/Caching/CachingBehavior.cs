156cb3aa08668a199c76fcc91d420aa926959b1d
Murat Ilgun <muratilgun34@gmail.com>
2024-03-10 05:20:57 +0300

    #

--- Core.Application/Pipelines/Caching/CachingBehavior.cs
+++ Core.Application/Pipelines/Caching/CachingBehavior.cs
@@ -1,55 +1,64 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
 using System.Text;
 using System.Text.Json;
 
 namespace Core.Application.Pipelines.Caching;
 public class CachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : IRequest<TRequest>, ICachableRequest
+    where TRequest : IRequest<TResponse>, ICachableRequest
 {
     private readonly CacheSettings _cacheSettings;
     private readonly IDistributedCache _cache;
+    private readonly ILogger<CachingBehavior<TRequest, TResponse>> _logger;
 
-    public CachingBehavior(IDistributedCache cache, IConfiguration configuration)
+    public CachingBehavior(IDistributedCache cache, ILogger<CachingBehavior<TRequest, TResponse>> logger, IConfiguration configuration)
     {
-        _cacheSettings = configuration.GetSection("CacheSettings")
-            .Get<CacheSettings>() ?? throw new InvalidOperationException("CacheSettings not found in appsettings.json");
+        _cacheSettings = configuration.GetSection("CacheSettings").Get<CacheSettings>() ?? throw new InvalidOperationException();
         _cache = cache;
+        _logger = logger;
     }
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         if (request.BypassCache)
+        {
             return await next();
+        }
 
         TResponse response;
         byte[]? cachedResponse = await _cache.GetAsync(request.CacheKey, cancellationToken);
-        if (cachedResponse is not null)
+        if (cachedResponse != null)
         {
             response = JsonSerializer.Deserialize<TResponse>(Encoding.Default.GetString(cachedResponse));
+            _logger.LogInformation($"Fetched from Cache -> {request.CacheKey}");
         }
         else
         {
             response = await GetResponseAndAddToCache(request, next, cancellationToken);
-
         }
 
         return response;
-
     }
 
-    private async Task<TResponse?> GetResponseAndAddToCache(TRequest request,
-        RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    private async Task<TResponse?> GetResponseAndAddToCache(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
     {
         TResponse response = await next();
 
         TimeSpan slidingExpiration = request.SlidingExpiration ?? TimeSpan.FromDays(_cacheSettings.SlidingExpiration);
         DistributedCacheEntryOptions cacheOptions = new() { SlidingExpiration = slidingExpiration };
 
-        byte[] serializedDate  = Encoding.Default.GetBytes(JsonSerializer.Serialize(response));
+        byte[] serializedData = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(response));
 
-        await _cache.SetAsync(request.CacheKey, serializedDate, cacheOptions, cancellationToken);
+        await _cache.SetAsync(request.CacheKey, serializedData, cacheOptions, cancellationToken);
+        _logger.LogInformation($"Added to Cache -> {request.CacheKey}");
+
+
 
         return response;
     }
